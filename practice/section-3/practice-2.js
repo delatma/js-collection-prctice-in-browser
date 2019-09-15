09fe2e6ba54ea3b8d602c2a68ad9486e839ecdfa
delatma <delatma@oocl.com>
2019-09-16 02:40:08 +0800

    update 'practice-2' of section-3

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var ctr = 1;
+	var arr = [];
+	for(var i = 0; i < collectionA.length; i++){
+		var cnt = collectionA[i].count;
+		for(var j = 0; j < objectB.value.length; j++){
+			if(collectionA[i].key === objectB.value[j]){
+				cnt -= ctr;
+				ctr++;
+				if(ctr > 3){
+					ctr = 1;
+				}
+			}
+		}
+		var map = new Map();
+		map.set('key', collectionA[i].key);
+		map.set('count', cnt);
+		arr.push(Object.fromEntries(map.entries()));
+	}	
+	return arr;
 }
