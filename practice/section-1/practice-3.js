59af6326917131975ae815a86d661c212e83411f
delatma <delatma@oocl.com>
2019-09-15 20:03:34 +0800

    update 'practice-3.js'

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var arr = [];
+	var newArr = [];
+	for (var key in objectB) {
+	    if (objectB.hasOwnProperty(key)) {
+	        arr.push(objectB[key]);
+	    }
+	};
+
+	for(var i = 0; i < arr.length; i++)
+	{
+	    newArr = newArr.concat(arr[i]);
+	}
+
+	return collectionA.filter(value => -1 !== newArr.indexOf(value));
 }
