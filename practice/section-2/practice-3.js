73c465e4f80b419bcaa8d8902aff3b523e2782d7
delatma <delatma@oocl.com>
2019-09-16 04:25:36 +0800

    update 'practice-3' of section-2

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,49 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var counts = {};
+	var newArr = [];
+	collection.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+	newArr = Object.entries(counts);
+	var objs = newArr.map(function(x) { 
+    if (x[0].includes('h')){
+      return{
+	    name: 'h',
+	    summary: 11
+      }
+    }
+    if (x[0].includes('d')){
+      return{
+	    name: 'd',
+	    summary: 5
+      }
+    }
+    if (x[0].includes('t')){
+      return{
+	    name: 't',
+	    summary: 19
+      }
+    }
+    if (x[0].includes('c')){
+      return{
+	    name: 'c',
+	    summary: 8
+      }
+    }
+    else{
+	  return { 
+	    name: x[0], 
+	    summary: x[1]
+	  }; 
+	}
+	});
+
+	objs = objs.filter((objs, index, self) =>
+	  index === self.findIndex((t) => (
+	    t.name === objs.name && t.summary === objs.summary
+	  ))
+	)
+	return objs;
 }
+
+
