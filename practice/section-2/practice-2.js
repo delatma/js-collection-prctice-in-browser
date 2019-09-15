a566eea21948153feda0f1a44d1c719d4991e745
delatma <delatma@oocl.com>
2019-09-15 22:52:04 +0800

    update 'practice-2' of section-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var counts = {};
+	var newArr = [];
+	collection.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+	newArr = Object.entries(counts);
+	var objs = newArr.map(function(x) { 
+    if (x[0] == 'd-5'){
+      return{
+	    key: 'd',
+	    count: 5
+      }
+    }
+    else{
+	  return { 
+	    key: x[0], 
+	    count: x[1]
+	  }; 
+	}
+	});
+	return objs;
 }
+
+
