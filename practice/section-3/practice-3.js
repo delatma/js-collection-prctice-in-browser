6229b7609169b0ac9069f2ba1aa4e5a2a61f58cb
delatma <delatma@oocl.com>
2019-09-16 03:07:08 +0800

    update 'practice-3' of section-3

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,36 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var counts = {};
+	var newArr = [];
+	var ctr = 1;
+	var arr = [];
+
+	collectionA.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+	newArr = Object.entries(counts);
+	var objs = newArr.map(function(x) { 
+	  return { 
+	    key: x[0], 
+	    count: x[1] 
+	  }; 
+	});
+
+	for(var i = 0; i < objs.length; i++){
+		var cnt = objs[i].count;
+		for(var j = 0; j < objectB.value.length; j++){
+			if(objs[i].key === objectB.value[j]){
+				cnt -= ctr;
+				ctr++;
+				if(ctr > 3){
+					ctr = 1;
+				}
+			}
+		}
+		var map = new Map();
+		map.set('key', objs[i].key);
+		map.set('count', cnt);
+		arr.push(Object.fromEntries(map.entries()));
+	}	
+	return arr;
 }
+
